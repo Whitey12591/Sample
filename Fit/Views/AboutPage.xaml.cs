50d025277bc43e71a3a23d123fdb34492651a583
White12591 <danwhite1022@gmail.com>
2017-11-19 19:54:12 -0600

    Initial Commit

---
+++ Fit/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Fit
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
